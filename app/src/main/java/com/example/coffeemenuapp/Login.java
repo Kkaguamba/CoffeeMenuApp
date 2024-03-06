8cffa9908971bddabb6b0268cb56aa85c665106d
Kkaguamba <kkaguamba@gmail.com>
2024-03-06 11:02:45 +0300

    CoffeeMenuApp

--- app/src/main/java/com/example/coffeemenuapp/Login.java
+++ app/src/main/java/com/example/coffeemenuapp/Login.java
@@ -89,7 +89,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onSuccess(AuthResult authResult) {
                 Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(Login.this, MainActivity.class);
+                Intent i = new Intent(Login.this, Dashboard.class);
                 startActivity(i);
                 finish();
 
